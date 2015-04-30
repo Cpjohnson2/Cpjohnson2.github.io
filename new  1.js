4c6f5b09d345fb4f38c6dcad1b8e48a82fcda029
unknown <Cjohnson_H@Dev06.bsecode.inc>
2015-04-30 14:12:40 -0500

    no message

---
+++ new  1.js
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Practice</title>
+  </head>
+  
+<body>
+
+<div class="container">
+	<div class="row">
+		<div class="col-md-12">
+			<center><h1>Hello world.</h1></center>
+		</div>
+	</div>
+</div>
+
+
+
+</body>
+</html>
