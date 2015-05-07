46a31f54fab18fb95be9b618059f2512e1357ae5
unknown <CJohnson_H@Dev06.bsecode.inc>
2015-05-07 13:10:13 -0500

    More testing....surprise, surprise

--- assets/js/blogger.js
+++ assets/js/blogger.js
@@ -3,6 +3,11 @@ function getposts(){
     $.getJSON('entries.json', function(data) {
 		$.each(data.entries, function(key, val) {
 				console.log(val.date);
+				console.log(val.text);
+				$("#blog").append("<p>" + val.text + "</p>");
+				$("#blog").append("<p>"+ val.date+ "</p>");
+				
+				// 
         });
 	});
 }
