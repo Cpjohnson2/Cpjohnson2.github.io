d2515369f8e48104d31688312cb41065faf3e4af
unknown <CJohnson_H@Dev06.bsecode.inc>
2015-05-07 10:28:14 -0500

    json files

---
+++ assets/js/blogger.js
@@ -0,0 +1,8 @@
+$(document).ready(getposts);
+function getposts(){
+    $.getJSON('entries.json', function(data) {
+		$.each(data.entries, function(key, val) {
+				console.log(key);
+        }
+     }
+}
