e855311af3b037fbf30eaa8db6938289401a50f9
unknown <CJohnson_H@Dev06.bsecode.inc>
2015-05-13 14:25:41 -0500

    test

---
+++ test/rookies.js
@@ -0,0 +1,71 @@
+function drafted(){
+	//here I want all the functions that are applicable to drafted players to be in here 
+	
+
+function collegestat(){
+	
+//this function pulls college stats from 2014 and 2013 NCAAF season
+	
+}
+
+function combinestat(){
+	//this function will display the stats from the NFL combine
+	
+}
+
+function projectedstat(){
+//this function will give the user projected stats, and projected fantasy points of every player that is searched	
+	
+}
+
+function compare(){
+	//in this function I would like to compare the stats of 2014 rookies and 2015 drafted rookies 
+
+}
+
+function experts(){
+	//this function serves as a way to display what the "experts" have been saying about searched player 
+}
+
+	
+function draftpick)(){
+	//this will give us what position the players (rookies & 2014 rookies) were drafted in
+}
+	
+function standing(){
+	//this function will call only the 2014 rookies and tell the user where they stand on a particular roster, or on the fantasy football rating
+}
+
+function search(){
+	//this function is for my serach query function 
+}
+
+}
+THESE ARE WEBSITES THAT I WILL USE FOR DATA 
+http://www.fantasycube.com/wp-content/uploads/2012/08/offense-generic-xls-screenshot.png?3b65c9
+http://dynastyleaguefootball.com/
+
+/*
+var drafted = {
+  collegestat: function (){},
+  combinestat : function (){},
+  projectedstat : function(){},
+  compare : function (){},
+  experts : function (){},
+  draftpick:function (){},
+  standing: function(){},
+};
+*/
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
